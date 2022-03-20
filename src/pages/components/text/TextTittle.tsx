4914ef11e60ee08e6dbb9ba2d1c273827e5e7508
CtoConnectattoo <96078129+CtoConnectattoo@users.noreply.github.com>
2022-03-20 12:48:18 +0100

    changes

--- src/pages/components/text/TextTittle.tsx
+++ src/pages/components/text/TextTittle.tsx
@@ -1,16 +1,15 @@
 import { useTranslation } from "react-i18next";
 
 interface IText {
-    text: string;
- }
- 
-const Tittle = ({text}:IText) => {
-    const { t } = useTranslation();
+  text: string;
+}
+
+ const Tittle = ({ text }: IText) => {
+  const { t } = useTranslation();
   return (
-      <>
-  <h1> {t(text)}</h1>
-                  </>
+      <h1> {t(text)}</h1>
   );
 };
 
-export  {Tittle} ;
+export { Tittle };
+
